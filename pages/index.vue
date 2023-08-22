36021cbdfa3d59badbfd7c63317e9f416fbb75b7
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-08-22 22:52:03 +0530

    chore(cleanup): remove distractions

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,9 @@
 <script setup lang="ts">
-const online = useOnline()
 </script>
 
 <template>
   <div>
     <Logos class="mb-6" />
-    <Suspense>
-      <ClientOnly>
-        <PageView v-if="online" />
-        <div v-else class="text-stone-800/80">
-          You're offline
-        </div>
-      </ClientOnly>
-      <template #fallback>
-        <div class="italic">
-          <span class="animate-pulse">Loading...</span>
-        </div>
-      </template>
-    </Suspense>
-    <InputEntry />
+    <FileUpload />
   </div>
 </template>
